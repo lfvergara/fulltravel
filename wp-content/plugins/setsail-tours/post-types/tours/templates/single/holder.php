19238bc7b289f0e215ab1b17acb10cd1016a549f
Federico Vergara <hu.ce.ro@gmail.com>
2022-01-17 13:09:22 -0300

    Quitando sidebar

--- wp-content/plugins/setsail-tours/post-types/tours/templates/single/holder.php
+++ wp-content/plugins/setsail-tours/post-types/tours/templates/single/holder.php
@@ -1,5 +1,5 @@
 <div class="qodef-container">
-	<div class="qodef-container-inner clearfix <?php echo esc_attr($holder_class) ?>">
+	<div class="qodef-container-inner clearfix <?php echo esc_attr($holder_class) ?>" style="padding-top: 0px !important;">
 		<?php echo setsail_tours_get_tour_module_template_part('single/single-item', 'tours', 'templates', '', $params); ?>
 	</div>
 </div>
